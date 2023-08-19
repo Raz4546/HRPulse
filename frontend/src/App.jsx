534243274ed293aef9d72bb0f44b84caadfaf4c4
Raz4546 <raznissimov56@gmail.com>
2023-08-19 15:03:28 +0300

    without sql bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Employee from "./components/Employee";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
+import Start from "./components/Start";
+import EmployeeLogin from "./components/EmployeeLogin";
+import EmployeeDetail from "./components/EmployeeDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create" element={<AddEmployee />} />
+          <Route path="/employeeEdit/:id" element={<EditEmployee />} />
+        </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/employeeLogin" element={<EmployeeLogin />} />
+        <Route path="/employeedetail/:id" element={<EmployeeDetail />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/EditEmployee.jsx
@@ -0,0 +1,112 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+function EditEmployee() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    address: "",
+    salary: "",
+  });
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/get/" + id)
+      .then((res) => {
+        setData({
+          ...data,
+          name: res.data.Result[0].name,
+          email: res.data.Result[0].email,
+          address: res.data.Result[0].address,
+          salary: res.data.Result[0].salary,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .put("http://localhost:8081/update/" + id, data)
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          navigate("/employee");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="d-flex flex-column align-items-center pt-4">
+      <h2>Update Employee</h2>
+      <form className="row g-3 w-50" onSubmit={handleSubmit}>
+        <div className="col-12">
+          <label htmlFor="inputName" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputName"
+            placeholder="Enter Name"
+            autoComplete="off"
+            onChange={(e) => setData({ ...data, name: e.target.value })}
+            value={data.name}
+          />
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputEmail4" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail4"
+            placeholder="Enter Email"
+            autoComplete="off"
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+            value={data.email}
+          />
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputSalary" className="form-label">
+            Salary
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputSalary"
+            placeholder="Enter Salary"
+            autoComplete="off"
+            onChange={(e) => setData({ ...data, salary: e.target.value })}
+            value={data.salary}
+          />
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress" className="form-label">
+            Address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            placeholder="1234 Main St"
+            autoComplete="off"
+            onChange={(e) => setData({ ...data, address: e.target.value })}
+            value={data.address}
+          />
+        </div>
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">
+            Update
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default EditEmployee;
